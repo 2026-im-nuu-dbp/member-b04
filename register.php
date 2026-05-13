20836713285d5fc08dc8c9a4c9c676f82415228c
曾珮珊 <U1333102@o365.nuu.edu.tw>
2026-05-13 11:32:03 +0800

    register

--- register.php
+++ register.php
@@ -1,50 +1,43 @@
 <?php
-  require_once("db_config.php");
+header('Content-Type: application/json; charset=utf-8'); // 宣告回傳格式為 JSON
+require_once("db_config.php");
 
-  //取得表單資料
-  $account = $_POST["account"];
-  $password = $_POST["password"];
-  $name = $_POST["name"];
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    echo json_encode(["status" => "error", "message" => "不允許的存取方式"]);
+    exit();
+}
 
+$account = trim($_POST["account"] ?? "");
+$password = $_POST["password"] ?? "";
+$nickname = trim($_POST["nickname"] ?? "");
+$favorite_color = $_POST["favorite_color"] ?? "";
 
-  //檢查帳號是否有人申請
-  $sql = "SELECT COUNT(*) FROM members2 WHERE account = ?";
-  $stmt = $pdo->prepare($sql);
-  $stmt->execute([$account]);
-  $exists = (int) $stmt->fetchColumn();
+if ($account === "" || $password === "") {
+    echo json_encode(["status" => "error", "message" => "帳號與密碼不能為空！"]);
+    exit();
+}
 
-  //如果帳號已經有人使用
-  if ($exists !== 0)
-  {
-    //顯示訊息要求使用者更換帳號名稱 -- 直接產生網頁的程式碼！
-    echo "<script type='text/javascript'>";
-    echo "alert('您所指定的帳號已經有人使用，請使用其它帳號');";
-    echo "history.back();";
-    echo "</script>";
-  }
+// 檢查帳號是否有人申請
+$sql = "SELECT COUNT(*) FROM membership WHERE account = ?";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$account]);
+$exists = (int) $stmt->fetchColumn();
 
-  //如果帳號沒人使用
-  else
-  {
-    //執行 SQL 命令，新增此帳號
-    $sql = "INSERT INTO members2 (account, password, name, sex, cellphone, email, comment)
-            VALUES (?, ?, ?, ?, ?, ?, ?)";
+if ($exists !== 0) {
+    // 這裡不再輸出 <script>，而是直接回傳錯誤狀態碼與訊息
+    echo json_encode(["status" => "exists", "message" => "您所指定的帳號已經有人使用，請使用其它帳號"]);
+    exit();
+} else {
+    $password_secure = password_hash($password, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO membership (account, password, nickname, favorite_color) VALUES (?, ?, ?, ?)";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute([$account, $password, $name, $sex, $cellphone, $email, $comment]);
-  }
-?>
-<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>新增帳號成功</title>
-  </head>
-  <body bgcolor="#FFFFFF">
-    <p align="center"><img src="Success.jpg">
-    <p align="center">恭喜您已經註冊成功了，您的資料如下：（請勿按重新整理鈕）<br>
-      帳號：<font color="#FF0000"><?php echo $account ?></font><br>
-      密碼：<font color="#FF0000"><?php echo $password ?></font><br>
-      請記下您的帳號及密碼，然後<a href="login.html">登入網站</a>。
-    </p>
-  </body>
-</html>
+    $stmt->execute([$account, $password_secure, $nickname, $favorite_color]);
+
+    // 註冊成功，將需要顯示的資料打包成 JSON 回傳
+    echo json_encode([
+        "status" => "success",
+        "account" => $account,
+        "password" => $password // 注意：實務上基於安全，通常不建議在註冊成功後再次回傳明文密碼
+    ]);
+    exit();
+}
